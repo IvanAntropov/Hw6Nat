af0c85f2de8457b8e0f32485f71f7d281bbc5401
Ivan <vana.antropov@gmail.com>
2022-12-15 21:07:24 +0500

    commit

---
+++ betaServer.py
@@ -0,0 +1,55 @@
+import socket, selectors, types
+
+host = "127.0.0.1"
+port = 6666
+
+sel = selectors.DefaultSelector()
+
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+lsock.bind((host, port))
+lsock.listen()
+print(f"Listening on {(host, port)}")
+lsock.setblocking(False)
+sel.register(lsock, selectors.EVENT_READ, data=None)
+
+def accept_wrapper(sock):
+    conn, addr = sock.accept()  
+    print(f"Accepted connection from {addr}")
+    conn.setblocking(False)
+    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(conn, events, data=data)
+    
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024) 
+        if recv_data:
+            data.outb += recv_data
+        else:
+            print(f"Closing connection to {data.addr}")
+            sel.unregister(sock)
+            sock.close()
+    if mask & selectors.EVENT_WRITE:
+        if data.outb:
+            print(f"\nFrom {data.addr}\nMessage: {data.outb!r} \n")
+            sent = sock.send(data.outb) 
+            data.outb = data.outb[sent:]
+            
+try:
+    while True:
+        events = sel.select(timeout=None)
+        for key, mask in events:
+            if key.data is None:
+                accept_wrapper(key.fileobj)
+            else:
+                service_connection(key, mask)
+except KeyboardInterrupt:
+    print("Caught keyboard interrupt, exiting")
+finally:
+    sel.close()
+    
+
+    
+    
---
+++ user3.py
@@ -0,0 +1,32 @@
+import socket
+import threading
+
+# HOST = "10.0.2.15"  
+HOST = "127.0.0.1"  
+PORT = 6666  
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    name = input()
+    s.send(name.encode("ascii")) 
+    
+
+    def recieving():
+        while True:
+            data_chunk = s.recv(1024)
+            if data_chunk:
+                print(data_chunk)
+                
+    def send():
+        out = input()
+        s.send(out.encode("ascii"))
+        return out 
+    
+    
+    rec_thread = threading.Thread(target=recieving)
+    rec_thread.start()
+    
+    while True:
+        if send() == "exit":
+            s.close()
